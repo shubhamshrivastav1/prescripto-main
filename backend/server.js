86f85b8f7b965e963cc61f1e7bdbce7ac3e161ef
Shubham Shrivastav <ssk06112000@gmail.com>
2026-09-07 01:24:17 +0530

    fix: update server listen method to bind to all network interfaces

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,5 @@ app.get("/", (req, res) => {
   res.send("API WORKING");
 });
 
-app.listen(port, () => console.log("Server started", port));
+// app.listen(port, () => console.log("Server started", port));
+app.listen(port, "0.0.0.0", () => console.log("Server started", port));
